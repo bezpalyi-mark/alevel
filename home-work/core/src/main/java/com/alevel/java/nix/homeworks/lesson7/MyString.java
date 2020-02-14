7f5b2e09b4dd3421fb1086a46e91cd8b250858d6
Mark <christmas2491@gmail.com>
2020-02-14 21:36:43 +0200

    upgrade

--- home-work/core/src/main/java/com/alevel/java/nix/homeworks/lesson7/MyString.java
+++ home-work/core/src/main/java/com/alevel/java/nix/homeworks/lesson7/MyString.java
@@ -2,7 +2,15 @@ package com.alevel.java.nix.homeworks.lesson7;
 
 public class MyString {
     public CharSequence reverse(CharSequence charSequence) {
-        StringBuilder stringBuilder = new StringBuilder(charSequence);
-        return stringBuilder.reverse();
+       StringBuilder stringBuilder = new StringBuilder(charSequence);
+        for(int i = 0, j = charSequence.length() - 1; i < charSequence.length() / 2; i++, j--) {
+            char one = charSequence.charAt(i);
+            char two = charSequence.charAt(j);
+            stringBuilder.delete(i, i+1);
+            stringBuilder.delete(j-1, j);
+            stringBuilder.insert(i, two);
+            stringBuilder.insert(j, one);
+        }
+        return stringBuilder;
     }
 }
