e7d0650939d3f9ef4819ec7f6a3f303cc1a2a8ad
Mark <christmas2491@gmail.com>
2020-02-15 15:48:08 +0200

    upgrade test for task2.1

--- home-work/module-one/src/test/java/com/alevel/java/nix/module1/Level2Tests.java
+++ home-work/module-one/src/test/java/com/alevel/java/nix/module1/Level2Tests.java
@@ -7,8 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Level2Tests {
     @Test
     public void assertIsValidString() {
-        String[] inputs = {"{[([])]}", "{([)]}", "", "{())}", "[(]", "{}}}", "()", "}{"};
-        boolean[] expected = {true, false, true, false, false, false, true, false};
+        String[] inputs = {"{[([])]}", "{([)]}", "", "{())}", "[(]", "{}}}", "()", "}{", "[](){}", "abc"};
+        boolean[] expected = {true, false, true, false, false, false, true, false, true, false};
         Level2 level2 = new Level2();
         for (int i = 0; i < expected.length; i++) {
             boolean actual = level2.isValidString(inputs[i]);
