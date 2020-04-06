ffc4960aa367614bc7708d410b2b2dc421d4e1d6
Mark <christmas2491@gmail.com>
2020-04-06 22:22:38 +0300

    remove imports

--- home-work/module-two/src/main/java/com/alevel/java/nix/module2/UniqueName.java
+++ home-work/module-two/src/main/java/com/alevel/java/nix/module2/UniqueName.java
@@ -2,14 +2,12 @@ package com.alevel.java.nix.module2;
 
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class UniqueName {
     String from(List<String> names) {
-        if(names.size() == 0) {
+        if (names.size() == 0) {
             return null;
         }
         LinkedHashMap<String, Integer> collect = names.stream()
@@ -20,8 +18,8 @@ public class UniqueName {
                                 Collectors.summingInt(name -> 1)
                         )
                 );
-        for(var entry : collect.entrySet()) {
-            if(entry.getValue() == 1) {
+        for (var entry : collect.entrySet()) {
+            if (entry.getValue() == 1) {
                 return entry.getKey();
             }
         }
